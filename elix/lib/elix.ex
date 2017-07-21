a5c18c8857e31a1f1e2ad267c6adff68f8b52c2e
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-07-21 13:28:38 +0100

    elixir initial

---
+++ elix/lib/elix.ex
@@ -0,0 +1,18 @@
+defmodule Elix do
+  @moduledoc """
+  Documentation for Elix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elix.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
