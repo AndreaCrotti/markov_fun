190066d6cdfd30263bcfa2dd2d2b24025c973ceb
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-07-26 17:45:24 +0100

    initial teest

--- markov-clj/test/markov_clj/core_test.clj
+++ markov-clj/test/markov_clj/core_test.clj
@@ -4,3 +4,12 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.clojure-test :refer [defspec]]))
+
+(t/deftest gen-probs-test
+  (t/testing "generate probabilities map"
+    (t/are [words probs]
+        (= probs (core/gen-probs words))
+
+      ["hello" "world"] {"hello" {"world" 1}}
+      ["this" "is" "this" "are"] {"this" {"is" 1/2, "are" 1/2}
+                                  "is" {"this" 1}})))
